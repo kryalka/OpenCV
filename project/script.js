23329ea397802329d6dbcd94279eb4fd553725b0
Sofia <sinitsinasofa@gmail.com>
2024-12-10 12:10:36 +0300

    Add files via upload

---
+++ project/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Примерный код, чтобы показать как можно добавить интерактивность
+    const installSection = document.getElementById('install');
+
+    installSection.addEventListener('click', function() {
+        alert("Скачайте OpenCV для начала работы с библиотекой!");
+    });
+});
