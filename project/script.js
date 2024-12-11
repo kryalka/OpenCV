9a343abc9fa10dbad7f4524c555a0af3ab5b6321
Sofia <sinitsinasofa@gmail.com>
2024-12-11 22:19:56 +0300

    Delete project directory

--- project/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Примерный код, чтобы показать как можно добавить интерактивность
-    const installSection = document.getElementById('install');
-
-    installSection.addEventListener('click', function() {
-        alert("Скачайте OpenCV для начала работы с библиотекой!");
-    });
-});
